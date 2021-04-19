dbb26a1957128a29ac7bac68431bf6c0a98265a3
Omar Ouhra <omarouhra@Omars-Mac-mini.local>
2021-04-19 05:34:56 +0000

    deploy

--- back_end/settings.py
+++ back_end/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import django_heroku
 
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +31,6 @@ SECRET_KEY = 'n&jmkx-*lq0d=6#!2tskvy+^rbpg8u#9it6zq0evqc=&ib7d^g'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
